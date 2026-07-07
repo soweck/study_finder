2b9d0ce97c40a2b543b0f37d3d317d81cdc50d74
Andrew <andrewmatthew137@gmail.com>
2026-07-07 20:48:46 +1000

    Add user fixture and corresponding tests for user model functionality

---
+++ src/model/__fixtures__/user-fixtures.js
@@ -0,0 +1,10 @@
+import User from "../user.js";
+const user = {
+  id: 1,
+  name: "Andrew",
+  friends: [2, 3, 4, 5, 6, 7, 10, 11, 12],
+};
+
+export const createUser = (overrides = {}) => {
+  return new User({ ...user, ...overrides });
+};
---
+++ src/model/user.test.js
@@ -0,0 +1,32 @@
+import { describe, expect, test } from "vitest";
+import User from "./study-session.js";
+import { createUser } from "./__fixtures__/user-fixtures.js";
+
+describe("Testing user model functionality", () => {
+  test("Testing user ID conversion from int to string", () => {
+    const user = createUser({ id: 1 });
+    expect(user.id).toBe("1");
+  });
+  test("Testing friend ID conversion from int to string", () => {
+    const user = createUser({ friends: [1, 2] });
+    expect(user.friends).toEqual(["1", "2"]);
+  });
+  test("Testing addFriend with string input", () => {
+    const user = createUser();
+    const initialFriends = [...user.friends];
+    user.addFriend("13");
+    expect(user.friends).toEqual([...initialFriends, "13"]);
+  });
+  test("Testing addFriend with number input expecting to be converted to string", () => {
+    const user = createUser();
+    const initialFriends = [...user.friends];
+    user.addFriend(13);
+    expect(user.friends).toEqual([...initialFriends, "13"]);
+  });
+  test("Testing addFriend with duplicate friend", () => {
+    const user = createUser();
+    const initialFriends = [...user.friends];
+    user.addFriend(initialFriends[0]);
+    expect(user.friends).toEqual(initialFriends);
+  });
+});
