2e5b341d0c69cda09cf6054550f346c76a73bea9
Andrew <andrewmatthew137@gmail.com>
2026-07-10 16:26:29 +1000

    refactor: Preparing AppStore for implementation

---
+++ src/store/AppStore.js
@@ -0,0 +1,9 @@
+// store/AppStore.js
+import { User } from "../models/User.js";
+import { StudySession } from "../models/StudySession.js";
+
+class AppStore {
+  constructor() {}
+}
+
+export const store = new AppStore();
